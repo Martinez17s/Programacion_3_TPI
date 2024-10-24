3d7baa03ef681c512205df0fc6344ca3c1569d30
Lorenzo Martinez <lmar190504@gmail.com>
2024-10-23 23:41:35 -0300

    Add Entities and Exception

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    internal class User
+    public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        public UserRole Role { get; set; }
+    }
 
+    public enum UserRole
+    {
+        Client,
+        Admin,
+        Professor,
     }
 }
