598023fea584dc2837f2df7c2573a5d54a7acc86
Lorenzo Martinez <lmar190504@gmail.com>
2024-10-24 00:18:43 -0300

    DTOs and Requests

---
+++ Application/DTOs/EnrollmentDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.DTOs
+{
+    public class EnrollmentDto
+    {
+        public int EnrollmentId { get; set } 
+        public int SubjectId { get; set }
+        public int ClientId { get; set; }
+        
+    }
+}
---
+++ Application/DTOs/Requests/EnrollmentCreateRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Requests
+{
+    public class EnrollmentCreateRequest
+    {
+        [Required]
+        public int SubjectId { get; set; }
+        [Required]
+        public int ClientId { get; set; }
+
+    }
+}
