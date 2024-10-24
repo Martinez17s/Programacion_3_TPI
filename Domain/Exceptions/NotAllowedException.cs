9484a68bc3406776415423d28dd89ad4e9f8c5ba
hsz <santinogz131@gmail.com>
2024-10-24 00:16:18 -0300

    Cambios pequeños

--- Domain/Exceptions/NotAllowedException.cs
+++ Domain/Exceptions/NotAllowedException.cs
@@ -8,7 +8,11 @@ namespace Domain.Exceptions
 {
     public class NotAllowedException : Exception
     {
+        public NotAllowedException() : base() { }
 
+        public NotAllowedException(string message) : base(message) { }
+
+        public NotAllowedException(string message, Exception innerException) : base(message, innerException) { }
 
     }
 }
